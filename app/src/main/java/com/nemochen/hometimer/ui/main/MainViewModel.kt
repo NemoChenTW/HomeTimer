08ef937ea70af6e193738279b77004f600854d04
NemoChen <nemochentw@gmail.com>
2020-06-09 00:33:47 +0800

    Add progress bar

--- app/src/main/java/com/nemochen/hometimer/ui/main/MainViewModel.kt
+++ app/src/main/java/com/nemochen/hometimer/ui/main/MainViewModel.kt
@@ -7,17 +7,20 @@ import com.nemochen.hometimer.model.DataModel
 
 class MainViewModel : ViewModel() {
 
+    var isLoading = ObservableField(false)
     var itemList = ObservableField<MutableList<CountdownElement>>()
     var displayString = ObservableField<String>()
 
     var dataModel = DataModel()
 
     fun getData() {
-        println("getData")
+
+        isLoading.set(true)
         dataModel.retrieveData(object : DataModel.onDataReadyCallback{
             override fun onDataReady(dataList: List<CountdownElement>) {
                 itemList.set(dataList as MutableList<CountdownElement>)
                 displayString.set(itemList.get()?.get(0)?.name)
+                isLoading.set(false)
             }
         })
     }
