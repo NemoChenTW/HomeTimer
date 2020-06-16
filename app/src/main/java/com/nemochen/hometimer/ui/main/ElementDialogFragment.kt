336d372842a3697a1d2fcac70d953129406ac31f
NemoChen <nemochentw@gmail.com>
2020-06-17 00:35:39 +0800

    Work for DatePicker

--- app/src/main/java/com/nemochen/hometimer/ui/main/ElementDialogFragment.kt
+++ app/src/main/java/com/nemochen/hometimer/ui/main/ElementDialogFragment.kt
@@ -1,7 +1,11 @@
 package com.nemochen.hometimer.ui.main
 
+import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.nemochen.hometimer.R
@@ -16,7 +20,24 @@ class ElementDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         context?.apply {
             val builder: AlertDialog.Builder = AlertDialog.Builder(this, 0)
-            builder.setView(activity?.layoutInflater?.inflate(R.layout.dialog_fragment, null))
+            val v = activity?.layoutInflater?.inflate(R.layout.dialog_fragment, null)
+            v?.findViewById<TextView>(R.id.date)?.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    DatePickerDialog(this@apply, object : DatePickerDialog.OnDateSetListener{
+                        override fun onDateSet(
+                            view: DatePicker?,
+                            year: Int,
+                            month: Int,
+                            dayOfMonth: Int
+                        ) {
+                            (v as TextView).text = "$year/$month/$dayOfMonth"
+                        }
+
+                    }, 2020, 6, 17).show()
+                }
+
+            })
+            builder.setView(v)
 
             return builder.create()
         }
