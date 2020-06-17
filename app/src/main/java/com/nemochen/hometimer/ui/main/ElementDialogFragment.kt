ff4b972f8369bd22a68d83ef2ef40130eed3a110
NemoChen <nemochentw@gmail.com>
2020-06-17 11:57:08 +0800

    User SimpleDateFormat for DatePickerDialog result

--- app/src/main/java/com/nemochen/hometimer/ui/main/ElementDialogFragment.kt
+++ app/src/main/java/com/nemochen/hometimer/ui/main/ElementDialogFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.nemochen.hometimer.databinding.DialogFragmentBinding
+import com.nemochen.hometimer.util.TimeDisplayUtil
 import java.util.*
 
 class ElementDialogFragment : DialogFragment() {
@@ -44,7 +45,9 @@ class ElementDialogFragment : DialogFragment() {
                 if (it != null) {
                     val calendar = Calendar.getInstance()
                     DatePickerDialog(this,
-                        DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth -> binding.date.text = "$year/$month/$dayOfMonth" },
+                        DatePickerDialog.OnDateSetListener {
+                                _, year, month, dayOfMonth -> binding.date.text = TimeDisplayUtil.dataFormater.format(Calendar.getInstance().apply { set(year, month, dayOfMonth) }.time)
+                        },
                         calendar.get(Calendar.YEAR),
                         calendar.get(Calendar.MONTH),
                         calendar.get(Calendar.DAY_OF_MONTH)
@@ -52,7 +55,6 @@ class ElementDialogFragment : DialogFragment() {
                 }
                 Calendar.YEAR
             }
-
         })
     }
 }
--- app/src/main/java/com/nemochen/hometimer/util/TimeDisplayUtil.kt
+++ app/src/main/java/com/nemochen/hometimer/util/TimeDisplayUtil.kt
@@ -1,11 +1,16 @@
 package com.nemochen.hometimer.util
 
+import java.text.SimpleDateFormat
+
 class TimeDisplayUtil {
     companion object {
         const val MINUTE_SECONDS = 60
         const val HOUR_SECONDS = MINUTE_SECONDS * 60
         const val DAY_SECONDS = HOUR_SECONDS * 24
         const val PADDING_CHAR = '0'
+
+        val dataFormater = SimpleDateFormat("yyyy/MM/dd")
+        val dataTimeFormater = SimpleDateFormat("yyyy/mm/dd HH:mm:ss")
 
         fun getDisplayString(remainingSeconds: Long): String {
             var rDays = if (remainingSeconds >= DAY_SECONDS) {remainingSeconds / DAY_SECONDS} else null
